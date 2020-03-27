f442304b75b4928544a95a7983d0e0590a2d704b
Александр Шевченко <sanya132009@gmail.com>
2020-03-27 16:59:08 +0200

    drag&drop; -jquery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,10 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-import * as $ from 'jquery';
-import {Subscription} from 'rxjs';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
-
-  sub$: Subscription;
-
-  constructor(public router: Router,
-  ) {
-  }
-
-  ngOnInit() {
-    this.sub$ = this.router.events.subscribe(() => {
-      if (this.router.url !== '/') {
-        $('body').css('background-color', '#4BBF6B');
-        $('.navbar').css('background-color', '#3FA25B');
-      } else {
-        $('body').css('background-color', '#fff');
-        $('.navbar').css('background-color', '#026AA7');
-      }
-    });
-  }
-
-  ngOnDestroy() {
-    this.sub$.unsubscribe();
-  }
+export class AppComponent {
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { HomePageComponent } from './home-page/home-page.component';
@@ -9,10 +8,8 @@ import {AppRoutingModule} from './app-routing.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import {DragDropModule} from '@angular/cdk/drag-drop';
-import { ModalDirective } from './directives/modal.directive';
 import { ButtonStyleDirective } from './directives/button-style.directive';
-import { FormStyleDirective } from './directives/form-style.directive';
-import {ModalCloseDirective} from './directives/modal-close.directive';
+import {StyleDirective} from './directives/style.directive';
 
 @NgModule({
   declarations: [
@@ -20,10 +17,8 @@ import {ModalCloseDirective} from './directives/modal-close.directive';
     MainLayoutComponent,
     HomePageComponent,
     BoardsPageComponent,
-    ModalDirective,
     ButtonStyleDirective,
-    FormStyleDirective,
-    ModalCloseDirective
+    StyleDirective
   ],
   imports: [
     BrowserModule,
